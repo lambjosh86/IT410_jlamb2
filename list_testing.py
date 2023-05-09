74321ac14833e5104cba0b6a72d3bd108b82ac7b
Josh Lamb <jlamb2@mail.walshcollege.edu>
2023-05-08 20:35:32 -0400

    Lists Demonstration

---
+++ list_testing.py
@@ -0,0 +1,8 @@
+favorite_marvel_movies = ["Avengers", "Thor:Ragnorok", "Captain America: Winter Soldier", "Guardians of the Galaxy"]
+
+print(favorite_marvel_movies)
+
+favorite_marvel_movies.sort()
+favorite_marvel_movies.reverse()
+
+print(favorite_marvel_movies)
