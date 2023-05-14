1603a8661bd7c4331af93c5af31309fa100567cf
Josh Lamb <jlamb2@mail.walshcollege.edu>
2023-05-14 19:00:17 -0400

    Try it Yourself Chapter 2

---
+++ TiY_Chapter2.py
@@ -0,0 +1,31 @@
+#Problem 2-1
+favorite_restaurant = "Outback Steakhouse"
+print(favorite_restaurant)
+
+favorite_dish = "Alice Springs Quesadilla"
+favorite_restaurant = favorite_dish
+print(favorite_restaurant)
+
+#Problem 2-2
+last_business = "Walmart"
+one_item = "Fidget Cube"
+print("The last time I went to " + last_business + " I purchased a " + one_item + ".")
+
+#Problem 2-3
+favorite_car = "Mustang"
+
+print(favorite_car.lower())
+print(favorite_car.upper())
+print(favorite_car.title())
+
+#Problem 2-4
+favorite_quote = "\t\nWhere Is My Super Suit?\t\n"
+print(favorite_quote)
+favorite_quote = "Where Is My Super Suit?".lstrip()
+print(favorite_quote)
+favorite_quote = "Where Is My Super Suit?".strip()
+print(favorite_quote)
+
+#Problem 2-5
+result = 2 + 3
+print("The result of my math problem is:", result)
