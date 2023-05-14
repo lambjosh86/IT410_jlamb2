35068e835f7277224a3def297e10e03cf8ec569f
Josh Lamb <jlamb2@mail.walshcollege.edu>
2023-05-14 19:02:53 -0400

    Variables and Simple Data Types Assignment

---
+++ variables_and_simple_data_types.py
@@ -0,0 +1,38 @@
+#step 1 - Store first name as a variable in lowercase letters
+first_name = "joshua"
+
+#step 2 - store last name as a variable in uppercase letters
+last_name = "LAMB"
+
+#step 3 - print greeting with modified first and last name
+print("Hello, " + first_name.upper() + " " + last_name.lower())
+
+#step 4 - print two newlines
+print("\n\n")
+
+#step 5 - print quote
+print('"Start by doing what\'s necessary; then do what\'s possible; and suddenly you are doing the impossible." - Francis of Assisi"')
+
+#step 6 - store decimal numbers as variables
+number_one = 2.5
+number_two = 4.4
+
+#step 7 - perform addition, subtraction, multiplication, and division on the two numbers
+addition = number_one + number_two
+subtraction = number_one - number_two
+multiplication = number_one * number_two
+division = number_one / number_two
+
+#step 8 - print results
+print(number_one, "plus", number_two, "equals", addition)
+print(number_one, "minus", number_two, "equals", subtraction)
+print(number_one, "times", number_two, "equals", multiplication)
+print(number_one, "divided by", number_two, "equals", division)
+
+#step 9 - store current day and month as variables
+current_month = "May"
+current_day = 10
+
+#step 10 - print the current date
+print("\n\t\tToday is day", current_day, "of the month of", current_month + ".")
+        
